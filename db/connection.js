4a5981334303d49fd3db04c0d958f163bcefd209
JohnCase38 <john.case296@gmail.com>
2022-10-22 10:40:52 -0400

    trying to add functionality

---
+++ db/connection.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+  
+    user: 'root',
+
+    password: 'password',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+db.query(`DELETE FROM employee_names WHERE id = ?`, 3, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+db.query('SELECT * FROM employee_names', function (err, results) {
+  console.log(results);
+});
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
